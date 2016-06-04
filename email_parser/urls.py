e61750c02515b07c3568b8c866a5aff684d370eb
Shubham Goyal <shubhamjigoyal@gmail.com>
2016-06-05 07:03:45 +0800

    Can receive emails on sandbox mailgun account and send text reply

--- email_parser/urls.py
+++ email_parser/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 
 urlpatterns = [
+	url(r'context-message', 'email_parser.views.process_hook_from_context'),
+	url(r'context-message/receipt', 'email_parser.views.process_receipt_message'),
     url(r'', 'email_parser.views.process_message'),
 ]
--- email_parser/views.py
+++ email_parser/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import requests
 
 # Create your views here.
 
 @csrf_exempt
 def process_message(request):
-	if request.method == 'POST':
+    if request.method == 'POST':
          sender    = request.POST.get('sender')
          print('Sender = ', sender)
          recipient = request.POST.get('recipient')
@@ -25,7 +26,26 @@ def process_message(request):
              file = request.FILES[key]
              print(file)
              # do something with the file
+         send_simple_message(sender, "Re: " + subject)
 
      # Returned text is ignored but HTTP status code matters:
      # Mailgun wants to see 2xx, otherwise it will make another attempt in 5 minutes
+         return HttpResponse('OK')
+
+def send_simple_message(recipient_email_id, subject):
+    return requests.post(
+        "https://api.mailgun.net/v3/sandboxaeedb52bbc344d3db562ce0ddc5fb584.mailgun.org/messages",
+        auth=("api", "key-ae6af4f4c6a04a3f48fa506094189246"),
+        # data={"from": "Billwise postmaster@sandboxaeedb52bbc344d3db562ce0ddc5fb584.mailgun.org",
+        data={"from": "Billwise super-smart-ai@api.billwise.co",      
+              "to": [recipient_email_id],
+              "subject": subject,
+              "text": "Hey, thanks for sending us your bill. We will parse it and add it to our knowledge base. Our bots are already at work using the new information to make your bill-paying existence better and more useful."})
+
+@csrf_exempt
+def process_hook_from_context(request):
+	return HttpResponse('OK')
+
+def process_receipt_message(request):
+	print("Receipt callback called")
 	return HttpResponse('OK')
